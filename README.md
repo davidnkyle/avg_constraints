89bc9b1a959d6303e6ab1de019024fc6cb5cef23
David Kyle <dkyle@westmont.edu>
2017-09-11 16:05:55 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # avg_constraints
 This repo contains the code for a project that finds a feasible solution to a budgeting problem that avoids restraint boundaries
+
+constraints2.py sets up the initial conditions for initial balance, savings, income, and expenses
+
+average.py computes a feasible solution to the set of linear constraints by finding an average of random points
+uniformly distributed accross the convex polytope.
+
+report.py prints the findings in the console.
--- constraints2.py
+++ constraints2.py
@@ -1,1 +1,41 @@
+def m2i(m1, m2= None):
+    if m2 == None:
+        return [(m1 - 9)%12]
+    return range((m1 - 9)%12, (m2 - 9)%12 + 1)
 
+##
+# initial balance fixed
+#
+ibfd = {'emilys checking and savings account': 10000,
+        'davids checking and savings account': 5000,
+        'mom and dad last semester fund':      4500
+        }
+init_bal = sum(ibfd.values())
+
+# initial balance variable (YAGNI)
+
+# savings fixed (YAGNI)
+
+##
+# savings variable
+#
+svd = {'two months rent': [4000, m2i(9,8)],
+       'extra months rent': [2000, m2i(8)]
+       }
+
+##
+# income fixed
+#
+ifd = {'northrop fall': [640, m2i(9,11)],
+       'northrop spring': [800, m2i(1,8)]
+       }
+
+# income variable (YAGNI)
+
+##
+# expenses fixed
+#
+
+##
+# expenses variable
+#
