511c7db46d30b43c02c45cb079a8e268d82bc90f
David Kyle <dkyle@westmont.edu>
2017-09-14 09:52:24 -0700

    getting csv files to write

--- average.py
+++ average.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.optimize as opt
+import pandas as pd
+import pickle
 
 # read in linear_bounds data
 with open('linear_bounds/bounds.txt', 'r') as fp:
@@ -9,28 +11,47 @@ b_ub = np.load('linear_bounds/b_ub.npy')
 
 number_of_variables = len(A_ub[1,:])
 
-cube_low = []
-cube_high = []
-for i in range(number_of_variables):
-    c_min = [0 for i in range(number_of_variables)]
-    c_max = [0 for i in range(number_of_variables)]
-    c_min[i] = 1
-    c_max[i] = -1
-    cube_low.append(opt.linprog(c_min, A_ub, b_ub, bounds=bounds).x[i])
-    cube_high.append(opt.linprog(c_max, A_ub, b_ub, bounds=bounds).x[i])
+status = opt.linprog([1]*number_of_variables, A_ub, b_ub, bounds=bounds).status
+if(status == 2):
+    print('infeasible')
+elif status == 0:
+    cube_low = []
+    cube_high = []
+    for i in range(number_of_variables):
+        c_min = [0 for i in range(number_of_variables)]
+        c_max = [0 for i in range(number_of_variables)]
+        c_min[i] = 1
+        c_max[i] = -1
+        cube_low.append(opt.linprog(c_min, A_ub, b_ub, bounds=bounds).x[i])
+        cube_high.append(opt.linprog(c_max, A_ub, b_ub, bounds=bounds).x[i])
 
-def find_center(A_ub, b_ub, cube_low, cube_high, iterations):
-    total = np.zeros(number_of_variables)
-    success = 0
-    for i in range(iterations):
-        x = np.random.uniform(cube_low, cube_high, number_of_variables)
-        if all(A_ub.dot(x) <= b_ub):
-            total += x
-            success += 1
-    if success > 100:
-        return total/success
-    raise(Exception('not a big enough sample size'))
+    def find_center(A_ub, b_ub, cube_low, cube_high, iterations):
+        total = np.zeros(number_of_variables)
+        success = 0
+        for i in range(iterations):
+            x = np.random.uniform(cube_low, cube_high, number_of_variables)
+            if all(A_ub.dot(x) <= b_ub):
+                total += x
+                success += 1
+        if success > 100:
+            return total/success
+        raise(Exception('not a big enough sample size'))
 
-center = find_center(A_ub, b_ub, cube_low, cube_high, 100000)
-print(center)
+    center = find_center(A_ub, b_ub, cube_low, cube_high, 100000)
+    print(center)
+
+    # savings files
+    def write_csv_matrix2(importname, labelname, exportname):
+        with open('labels/'+labelname, 'rb') as f:
+            lab = pickle.load(f)
+        mx = np.load(importname + '.npy')
+        mx = (mx.T * center[:mx.shape[0]]).T
+        mx = np.vstack(([['sep', 'oct', 'nov', 'dec', 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug']], mx))
+        mx = np.vstack(([0] + lab, mx.T)).T
+        df = pd.DataFrame(mx)
+        df.to_csv(exportname + ".csv")
+
+    write_csv_matrix2('evmx', 'evlabels', 'evmx')
+    write_csv_matrix2('svmx', 'svlabels', 'svmx')
+
 
--- constraints2.py
+++ constraints2.py
@@ -1,16 +1,25 @@
 import numpy as np
 import pickle
+import pandas as pd
 
 def m2i(m1, m2= None):
     if m2 == None:
         return [(m1 - 9)%12]
     return range((m1 - 9)%12, (m2 - 9)%12 + 1)
 
+def i2b(ls, n):
+    x = np.zeros(12)
+    for index in ls:
+        x[index] = 1
+    for i in range(1, 12):
+        x[i] = n*x[i-1] + x[i]
+    return x
+
 ##
 # initial balance fixed
 #
 ibfd = {'emilys checking and savings account': 10000,
-        'davids checking and savings account': 5000,
+        'davids checking and savings account': 10000,
         'mom and dad last semester fund':      4500
         }
 init_bal = sum(ibfd.values())
@@ -25,27 +34,32 @@ init_bal = sum(ibfd.values())
 svd = {'two months rent': [4000, m2i(9,8)],
        'extra months rent': [2000, m2i(8)]
        }
-svlabels = []
-svvalues = []
-for element in svd:
-    svlabels.append(element)
-    svvalues.append(svd[element])
 with open('labels/svlabels', 'wb') as fp:
-    pickle.dump(svlabels, fp)
+    pickle.dump(sorted(svd), fp)
+
+def write_npy_matrix(filename, dictionary):
+    matrix = np.matrix([i2b(dictionary[label][1], 0) for label in dictionary])
+    np.save(filename + '.npy', matrix)
+
+write_npy_matrix('svmx', svd)
 
 ##
 # income fixed
 #
-ifd = {'northrop fall': [640, m2i(9,11)],
-       'northrop spring': [800, m2i(1,8)]
+ifd = {'northrop fall': [3000, m2i(9,11)],
+       'northrop spring': [3000, m2i(1,8)]
        }
-iflabels = []
-ifvalues = []
-for element in ifd:
-    iflabels.append(element)
-    ifvalues.append(ifd[element])
 with open('labels/iflabels', 'wb') as fp:
-    pickle.dump(iflabels, fp)
+    pickle.dump(sorted(svd), fp)
+
+def write_csv_matrix(filename, dictionary):
+    matrix = np.matrix([dictionary[label][0]*i2b(dictionary[label][1],0) for label in dictionary])
+    matrix = np.vstack(([['sep','oct','nov','dec','jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug']], matrix))
+    matrix = np.vstack(([init_bal]+sorted(dictionary),matrix.T)).T
+    df = pd.DataFrame(matrix)
+    df.to_csv(filename + ".csv")
+
+write_csv_matrix('ifmx', ifd)
 
 # income variable (YAGNI)
 
@@ -55,13 +69,9 @@ with open('labels/iflabels', 'wb') as fp:
 efd = {'gas and grocery': [1200, range(12)],
        'UCSB tuition': [4000, m2i(9) + m2i(1) + m2i(5)]
        }
-eflabels = []
-efvalues = []
-for element in efd:
-    eflabels.append(element)
-    efvalues.append(efd[element])
 with open('labels/eflabels', 'wb') as fp:
-    pickle.dump(eflabels, fp)
+    pickle.dump(sorted(efd), fp)
+write_csv_matrix('efmx', efd)
 
 ##
 # expenses variable
@@ -71,31 +81,20 @@ evd = {'rent': [1300, m2i(11, 8)],
        'vacation': [0, m2i(6) + m2i(8)],
        'eating out': [200, range(12)]
        }
-evlabels = []
-evvalues = []
-for element in evd:
-    evlabels.append(element)
-    evvalues.append(evd[element])
 with open('labels/evlabels', 'wb') as fp:
-    pickle.dump(evlabels, fp)
+    pickle.dump(sorted(evd), fp)
+
+write_npy_matrix('evmx', evd)
 
 ##
 # A_ub
 #
 
-def i2b(ls, n):
-    x = np.zeros(12)
-    for index in ls:
-        x[index] = 1
-    for i in range(1, 12):
-        x[i] = n*x[i-1] + x[i]
-    return x
-
-A_ub = np.array(i2b(evvalues[0][1], 1))
-for x in evvalues[1:]:
-    A_ub = np.vstack((A_ub, i2b(x[1], 1)))
-for x in svvalues:
-    A_ub = np.vstack((A_ub, i2b(x[1], 0)))
+A_ub = np.array(i2b(evd[sorted(evd)[0]][1], 1))
+for x in sorted(evd)[1:]:
+    A_ub = np.vstack((A_ub, i2b(evd[x][1], 1)))
+for x in sorted(svd):
+    A_ub = np.vstack((A_ub, i2b(svd[x][1], 0)))
 A_ub = A_ub.T
 # write the file
 np.save('linear_bounds/A_ub.npy', A_ub)
@@ -106,19 +105,17 @@ np.save('linear_bounds/A_ub.npy', A_ub)
 
 b_ub = np.full(12, init_bal, dtype=float)
 
-for x in ifvalues:
-    b_ub += x[0]*i2b(x[1], 1)
-for x in efvalues:
-    b_ub -= x[0]*i2b(x[1], 1)
+for label in ifd:
+    b_ub += ifd[label][0]*i2b(ifd[label][1], 1)
+for label in efd:
+    b_ub += efd[label][0] * i2b(efd[label][1], 1)
 # write the file
 np.save('linear_bounds/b_ub.npy', b_ub)
 
 ##
 # bounds
 #
-bounds = []
-for x in evvalues + svvalues:
-    bounds.append((x[0], float('inf')))
+bounds = [(evd[l][0], float('inf')) for l in sorted(evd)] + [(svd[l][0], float('inf')) for l in sorted(svd)]
 # write the file
 with open('linear_bounds/bounds.txt', 'w') as fp:
     fp.write('\n'.join('%s %s' % x for x in bounds))
